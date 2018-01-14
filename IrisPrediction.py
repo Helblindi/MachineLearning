0b5375b1027d3a9f1f61a3af1a337ad6a5d2b330
msheridan <mspencersheridan@gmail.com>
2018-01-13 20:49:34 -0700

    Put everything into functions

--- IrisPrediction.py
+++ IrisPrediction.py
@@ -18,45 +18,54 @@ def get_accuracy(predicted, expected):
     return (total - incorrect) / total
 
 
-iris = datasets.load_iris()
+def run_gaussian_nb(train, test):
+    # Part 3: Use an existing algorithm to create a model
+    classifier = GaussianNB()
+    model = classifier.fit(train[:, :-1], train[:, -1])
 
-# Above and beyond
-try:
-    with open("FlowerData.txt") as f:
-        FlowerData = f.read()
-except FileNotFoundError:
-    FlowerData = None
+    # Part 4: Use model to make predictions
+    targets_predicted = model.predict(test[:, :-1])
 
-print(iris)
-# Show the data (the attributes of each instance)
-# print(iris.data)
+    accuracy = get_accuracy(targets_predicted, test[:, -1])
+    print("GaussianNB Accuracy: ", accuracy)
 
-# Show the target values (in numeric format) of each instance
-# print(iris.target)
 
-# Show the actual target names that correspond to each number
-# print(iris.target_names)
+def run_hard_coded_classifier(train, test):
+    # Part 5: Implement own  new Algorithm
+    classifier = HardCodedClassifier()
+    model = classifier.fit(train[:, :-1], train[:, -1])
+    hc_targets_predicted = model.predict(test[:, :-1])
 
-# put the data and the target values together
-data_set = np.column_stack((iris.data, iris.target))
+    hc_accuracy = get_accuracy(hc_targets_predicted, test[:, -1])
+    print("Hard Coded Classifier Accuracy: ", hc_accuracy)
 
-# Get the train data and the test data
-train_data, test_data = train_test_split(data_set, train_size=0.7, test_size=0.3)
 
-# Part 3: Use an existing algorithm to create a model
-classifier = GaussianNB()
-model = classifier.fit(train_data[:, :-1], train_data[:, -1])
+def main():
+    iris = datasets.load_iris()
 
-# Part 4: Use model to make predictions
-targets_predicted = model.predict(test_data[:, :-1])
+    # put the data and the target values together
+    data_set = np.column_stack((iris.data, iris.target))
 
-accuracy = get_accuracy(targets_predicted, test_data[:, -1])
-print("GaussianNB Accuracy: ", accuracy)
+    # Get the train data and the test data
+    train_data, test_data = train_test_split(data_set, train_size=0.7, test_size=0.3)
 
-# Part 5: Implement own  new Algorithm
-classifier = HardCodedClassifier()
-model = classifier.fit(train_data[:, :-1], train_data[:, -1])
-hc_targets_predicted = model.predict(test_data[:, :-1])
+    # Display available options to the user
+    print("Available predictive methods:")
+    print("Enter 1 for GaussianNB")
+    print("Enter 2 for HardCodedClassifier method")
+    print()
+    choice = input("Which predictive method would you like to use:")
 
-hc_accuracy = get_accuracy(hc_targets_predicted, test_data[:, -1])
-print("Hard Coded Classifier Accuracy: ", hc_accuracy)
+    # Execute a function dependent on user input
+    if choice == "1":
+        run_gaussian_nb(train_data, test_data)
+    elif choice == "2":
+        run_hard_coded_classifier(train_data, test_data)
+    else:
+        print("Invalid input.")
+
+
+# While not required, it is considered good practice to have
+# a main function and use this syntax to call it.
+if __name__ == "__main__":
+    main()
