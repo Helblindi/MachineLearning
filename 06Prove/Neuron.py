69073f97f90982f4dacd5a9a5c26b56a4d280e1e
msheridan <mspencersheridan@gmail.com>
2018-02-13 18:19:06 -0700

    Initializing assignment of 06Prove

---
+++ 06Prove/Neuron.py
@@ -0,0 +1,27 @@
+# definition for our Neuron Class
+class Neuron:
+    def __init__(self, weights=None):
+        return self
+
+
+"""
+This Weeks Assignment
+1) Create some type of data structure to hold a node (a.k.a. neuron).
+
+2) Store the set of input weights for each node.
+
+3) Provide a way to create a layer of nodes of any number (this should be easily specified via a parameter).
+
+4) Account for a bias input.
+
+5) Be able to take input from a dataset instance (with an arbitrary number of attributes) and have each node produce an output (i.e., 0 or 1) according to its weights.
+
+6) Be able to load and process at least the following two datasets:
+
+    - Iris (You didn't think we could leave this out did you!)
+
+    - Pima Indian Diabetes
+
+7) You should appropriately normalize each data set.
+"""
+# end of Neuron Class
